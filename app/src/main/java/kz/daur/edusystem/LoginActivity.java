cdcdcb9d9fac15c84dcf53a0b8f2607c1155e5f1
DaurF <213053@astanait.edu.kz>
2022-04-20 15:08:58 +0600

    Added regex for email and password lines. Added shadows for buttons. Added temporary logo in "welcome" and "login" activities.

--- app/src/main/java/kz/daur/edusystem/LoginActivity.java
+++ app/src/main/java/kz/daur/edusystem/LoginActivity.java
@@ -2,13 +2,71 @@ package kz.daur.edusystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText etEmail;
+    EditText etPass;
+    Button btnLogin;
+
+    final String EMAIL_PATTERN = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPass = (EditText) findViewById(R.id.etPass);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (etEmail.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(),
+                            "Please, enter email address", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (etEmail.getText().toString().trim().matches(EMAIL_PATTERN)) {
+                        // code
+                        checkPass(etPass);
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Invalid email address", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+
+    private void checkPass(EditText etPass) {
+        if (isValidPassword(etPass.getText().toString().trim())) {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(getApplicationContext(), "digit must occur at least once\n" +
+                    "special character, lower and upper case letter must occur at least once\n" +
+                    "no whitespace allowed", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private boolean isValidPassword(String password) {
+        Pattern pattern;
+        Matcher matcher;
+
+        final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{4,}$";
+
+        pattern = Pattern.compile(PASSWORD_PATTERN);
+        matcher = pattern.matcher(password);
+
+        return matcher.matches();
     }
 }
